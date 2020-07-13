2b9e59b5bb25f1622ce386fffa3f91f282f95cce
Elena P <elena.pan.ep@gmail.com>
2020-07-13 13:58:04 -0600

    Update favicon

---
+++ client/build/precache-manifest.65e397b70f26a7b0d132689e2ea9b9f7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca99cda4c4b580f98408a963fb44592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35d2a0c795be8d47549",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5854e18cefd546c4730c",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c35d2a0c795be8d47549",
+    "url": "/static/js/2.b50f0649.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/static/js/2.b50f0649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5854e18cefd546c4730c",
+    "url": "/static/js/main.5de0852f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "901b798afcbfafca31c67c653a15c614",
+    "url": "/static/media/food-dome.901b798a.jpg"
+  },
+  {
+    "revision": "94ea446038ee62a143707f47c6ce3ac3",
+    "url": "/static/media/header.94ea4460.png"
+  },
+  {
+    "revision": "55e0f0323d38c70785900c26c60de572",
+    "url": "/static/media/logo-white.55e0f032.svg"
+  },
+  {
+    "revision": "5fcf74c9371d935388a5067d43181a59",
+    "url": "/static/media/logo.5fcf74c9.svg"
+  },
+  {
+    "revision": "1deeebde906a6e31b63b229e412dd1c7",
+    "url": "/static/media/profile.1deeebde.png"
+  }
+]);
